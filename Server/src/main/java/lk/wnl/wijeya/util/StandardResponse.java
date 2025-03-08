2c85dc33e8f2c5166c41b26adca35cf994ef9dfc
Pamod460 <pamodkaushika@gmail.com>
2025-03-08 23:45:28 +0530

    feat: add custom exception handling

---
+++ Server/src/main/java/lk/wnl/wijeya/util/StandardResponse.java
@@ -0,0 +1,14 @@
+package lk.wnl.wijeya.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandardResponse {
+    private Integer code;
+    private String message;
+    private Object data;
+}
