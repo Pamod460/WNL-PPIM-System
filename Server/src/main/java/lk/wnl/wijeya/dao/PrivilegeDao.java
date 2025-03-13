dda4c7f460c794ef8d67f4fe18bae6247240e081
Pamod460 <pamodkaushika@gmail.com>
2025-03-13 14:04:06 +0530

    refactor : remove unused code lines and add custom exeptionn handling

--- Server/src/main/java/lk/wnl/wijeya/dao/PrivilegeDao.java
+++ Server/src/main/java/lk/wnl/wijeya/dao/PrivilegeDao.java
@@ -18,4 +18,6 @@ public interface PrivilegeDao extends JpaRepository<Privilege,Integer> {
     Privilege findByMyId(@Param("id") Integer id);
 
     boolean existsByRoleAndModuleAndOperation(Role role, Module module, Operation operation);
+
+    boolean existsByRoleAndModuleAndOperationAndIdNot(Role role, Module module, Operation operation, Integer id);
 }
