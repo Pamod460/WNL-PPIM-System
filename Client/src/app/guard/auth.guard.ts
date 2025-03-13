9685f7a8b4494463a4ad8f4e7bea06eb20ae12fb
Pamod460 <pamodkaushika@gmail.com>
2025-03-13 21:33:17 +0530

    Update auth.guard.ts

--- Client/src/app/guard/auth.guard.ts
+++ Client/src/app/guard/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import {AuthorizationManager} from "../service/authorizationmanager";
+import {AuthorizationManager} from "../service/auth/authorizationmanager";
 
 
 @Injectable({
