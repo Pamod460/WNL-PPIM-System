26a7217ba97179aaf685d46cf00c3e83d1610079
Pamod460 <pamodkaushika@gmail.com>
2025-03-17 00:12:45 +0530

    Refactor: Add custom constructor to StandardResponse

--- Server/src/main/java/lk/wnl/wijeya/util/StandardResponse.java
+++ Server/src/main/java/lk/wnl/wijeya/util/StandardResponse.java
@@ -2,13 +2,20 @@ package lk.wnl.wijeya.util;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class StandardResponse {
     private Integer code;
     private String message;
     private Object data;
+
+    public StandardResponse(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
 }
