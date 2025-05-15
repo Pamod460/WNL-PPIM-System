a9264139fb0c3e3e151a1ffda6946cd3715a28e9
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-05-15 11:28:49 +0530

    feat(client): Add model classes for Product Module

---
+++ Client/src/app/entity/Product.ts
@@ -0,0 +1,21 @@
+import {ProductStatus} from "./ProductStatus";
+import {ProductCategory} from "./ProductCategory";
+import {ProductFrequency} from "./ProductFrequency";
+import {ProductMaterial} from "./ProductMaterial";
+import {ProductPaper} from "./ProductPaper";
+
+export class Product{
+  id?: number;
+  code?: string;
+  name?: string;
+  quentity?: number;
+  dointroduced?: Date;
+  unitprice?: number;
+  description?: string;
+  photo?: Uint8Array | string; // could be base64 string from API
+  productStatus?: ProductStatus;
+  productCategory?: ProductCategory;
+  productfrequency?: ProductFrequency;
+  productMaterials?: ProductMaterial[];
+  productPapers?: ProductPaper[];
+}
---
+++ Client/src/app/entity/ProductFrequency.ts
@@ -0,0 +1,4 @@
+export interface ProductFrequency{
+  id: number;
+  frequency: string;
+}
