e5e02aa8b6af6af44885d4f87fc8ccc017c98347
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-05-22 19:32:56 +0530

    feat(client): Register Paper component in app module and set up routing in app routing module

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import {AuthGuard} from "./guard/auth.guard";
 import {MaterialComponent} from "./view/modules/material/material.component";
 import {SupplierComponent} from "./view/modules/supplier/supplier.component";
 import {ProductComponent} from "./view/modules/product/product.component";
+import {PaperComponent} from "./view/modules/paper/paper.component";
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
@@ -30,6 +31,7 @@ const routes: Routes = [
       {path: "material", component: MaterialComponent},
       {path: "supplier", component: SupplierComponent},
       {path: "product", component: ProductComponent},
+      {path: "paper", component: PaperComponent},
       {path: "reports", component: ArrearsByProgramComponent},
     ],canActivate:[AuthGuard]
   }];
