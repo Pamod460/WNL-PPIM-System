638e12a691db2dc06072477f1757d963dc009d30
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-05-24 16:43:14 +0530

    refactor(server): add regex for Agent module

--- Server/src/main/java/lk/wnl/wijeya/dto/AgentDto.java
+++ Server/src/main/java/lk/wnl/wijeya/dto/AgentDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -14,12 +15,18 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class AgentDto {
     Integer id;
+    @Pattern(regexp = "^AG\\d{3}$", message = "Invalid Number")
     String number;
+    @Pattern(regexp = "^(([\\d]{9}[vVxX])|([\\d]{12}))$", message = "Invalid NIC")
     String nic;
+    @Pattern(regexp = "^([A-Z][a-z]+)$", message = "Invalid Calligname")
     String fullName;
+    @Pattern(regexp = "^((\\+94|0)(70|71|72|74|75|76|77|78)\\d{7})$", message = "Invalid Mobile Number")
     String mobile;
+    @Pattern(regexp = "^(0(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|51|52|54|55|57|61|63|65|66|67|71|81|91)-?[0-9]{7}?)$", message = "Invalid Landphone Number")
     String land;
     String address;
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Invalid Email")
     String email;
     LocalDate doRegisterd;
     String description;
