d12b432efe245013c39e0006baa307a09e77fa47
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-05-25 11:16:44 +0530

    feat(server): Add DTO classes for Vehicle module

---
+++ Server/src/main/java/lk/wnl/wijeya/dto/VehicleBrandDto.java
@@ -0,0 +1,17 @@
+package lk.wnl.wijeya.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class VehicleBrandDto {
+    private Integer id;
+    private String name;
+
+}
