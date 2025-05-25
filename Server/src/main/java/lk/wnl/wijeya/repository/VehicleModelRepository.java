9deac9abd3ae69e87c8602ab6f17422ff4194357
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-05-25 11:17:46 +0530

    feat(server): Add Repository classes for Vehicle Module

---
+++ Server/src/main/java/lk/wnl/wijeya/repository/VehicleModelRepository.java
@@ -0,0 +1,8 @@
+package lk.wnl.wijeya.repository;
+
+
+import lk.wnl.wijeya.entity.VehicleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleModelRepository extends JpaRepository<VehicleModel,Integer> {
+}
---
+++ Server/src/main/java/lk/wnl/wijeya/repository/VehicleStatusRepository.java
@@ -0,0 +1,7 @@
+package lk.wnl.wijeya.repository;
+
+import lk.wnl.wijeya.entity.VehicleStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleStatusRepository extends JpaRepository<VehicleStatus,Integer> {
+}
