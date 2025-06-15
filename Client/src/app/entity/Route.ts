55657097a458037d4777a365c50a190744e954f6
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-06-15 22:30:03 +0530

    feat(client): Add model classes for Route Module

--- Client/src/app/entity/Route.ts
+++ Client/src/app/entity/Route.ts
@@ -1,4 +1,14 @@
-export interface Route{
-  id: number;
-  name: string;
+import {RouteStatus} from "./RouteStatus'";
+
+export class Route{
+  id!: number;
+  name?: string;
+  map?: Uint8Array;
+  distance?: number;
+  estimatedTime?: number;
+  description?: string;
+  assignedDate?: string;
+  routeStatus?: RouteStatus;
+  routeNumber?: string;
+  logger?: string;
 }
