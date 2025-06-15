884539489853f6b16d9ad4adca574dce37c4c79a
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-06-15 22:26:49 +0530

    feat(server): Add DTO classes for Route Module

--- Server/src/main/java/lk/wnl/wijeya/dto/RouteDto.java
+++ Server/src/main/java/lk/wnl/wijeya/dto/RouteDto.java
@@ -1,9 +1,12 @@
 package lk.wnl.wijeya.dto;
 
+import lk.wnl.wijeya.entity.RouteStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 
 @Getter
@@ -11,6 +14,28 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RouteDto {
-    Integer id;
-    String name;
+
+    private Integer id;
+
+    private String name;
+
+    private byte[] map;
+
+    private Integer distance;
+
+    private Double estimatedTime;
+
+    private String description;
+
+    private LocalDate assignedDate;
+
+    private RouteStatus routeStatus;
+    private String routeNumber;
+    private String logger;
+
+    public RouteDto(Integer id, String routeNumber, String name) {
+        this.id = id;
+        this.routeNumber = routeNumber;
+        this.name = name;
+    }
 }
---
+++ Server/src/main/java/lk/wnl/wijeya/dto/RouteStatusDto.java
@@ -0,0 +1,22 @@
+package lk.wnl.wijeya.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RouteStatusDto {
+    private Integer id;
+    private String name;
+
+}
