dfe925dccbe07cf6d88e4b30ae3e1e5011b8c95e
Pamod460 <109800446+Pamod460@users.noreply.github.com>
2025-06-15 22:27:23 +0530

    feat(server): Add Repository classes for Route Module

--- Server/src/main/java/lk/wnl/wijeya/repository/RouteRepository.java
+++ Server/src/main/java/lk/wnl/wijeya/repository/RouteRepository.java
@@ -1,8 +1,12 @@
 package lk.wnl.wijeya.repository;
 
-import lk.wnl.wijeya.entity.District;
+import lk.wnl.wijeya.entity.Employee;
 import lk.wnl.wijeya.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouteRepository extends JpaRepository<Route,Integer> {
+
+    Boolean existsByRouteNumber(String routeNumber);
+
+    Route findTopByOrderByIdDesc();
 }
