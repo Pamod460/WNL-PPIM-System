f67895d43aeac59fdcd877a2616eeb535d7ed445
pamod kaushika <109800446+Pamod460@users.noreply.github.com>
2025-07-04 21:12:42 +0530

    feat(server): Add entity classes for  Material GRN  Module

---
+++ Server/src/main/java/lk/wnl/wijeya/entity/MaterialGrnMaterial.java
@@ -0,0 +1,40 @@
+package lk.wnl.wijeya.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "material_grn_material")
+public class MaterialGrnMaterial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "line_cost", precision = 7, scale = 2)
+    private BigDecimal lineCost;
+
+    @Column(name = "unit_cost", precision = 7, scale = 2)
+    private BigDecimal unitCost;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+    @JsonIgnore
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "material_grn_id", nullable = false)
+    private MaterialGrn materialGrn;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "material_id", nullable = false)
+    private Material material;
+
+}
